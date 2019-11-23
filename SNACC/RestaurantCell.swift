d6ccedaa627f21beaf8d933f09c85b1807220a47
David <lin.david985@gmail.com>
2019-11-23 11:44:21 -0500

    edited view controllers

---
+++ SNACC/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  SNACC
+//
+//  Created by David Lin on 11/23/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
