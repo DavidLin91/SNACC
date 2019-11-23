4fb866fbeec8db588b9b75c1510b46aa4b91e44f
David <lin.david985@gmail.com>
2019-11-23 10:59:38 -0500

    Initial Commit

---
+++ SNACC/ExploreVC.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SNACC
+//
+//  Created by David Lin on 11/23/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SNACC/ProfileVC.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SNACC
+//
+//  Created by David Lin on 11/23/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
