cba0cccf4e4dc594bd53cd6472ec5868f33474ae
David <lin.david985@gmail.com>
2019-12-22 22:40:15 -0500

    added view controller for bookmarks

---
+++ Profile View/BookmarkCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarkCell.swift
+//  SNACC
+//
+//  Created by David Lin on 12/22/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class BookmarkCell: UITableViewCell {
+
+}
---
+++ Profile View/BookmarkVC.swift
@@ -0,0 +1,18 @@
+//
+//  BookmarkVC.swift
+//  SNACC
+//
+//  Created by David Lin on 12/22/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class BookmarkVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
