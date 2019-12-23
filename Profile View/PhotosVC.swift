2aa2c1af4951ce061b45891cd5d440035f242844
David <lin.david985@gmail.com>
2019-12-23 13:38:24 -0500

    updated additional view controllers for profile tab bar

---
+++ Profile View/PhotosVC.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosVC.swift
+//  SNACC
+//
+//  Created by David Lin on 12/23/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class PhotosVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Profile View/ProfileLoyaltyVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoyaltyCardsVC.swift
+//  SNACC
+//
+//  Created by David Lin on 12/23/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ProfileLoyaltyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
