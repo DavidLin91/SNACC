0830f66e6a44984941e2eefef2bc903b4c8dc8a6
David <lin.david985@gmail.com>
2019-12-24 01:41:15 -0500

    added working address to map

--- Model/Restaurants.swift
+++ Model/Restaurants.swift
@@ -7,3 +7,7 @@
 //
 
 import Foundation
+
+struct Restaurants: Codable {
+    
+}
--- Restaurants View/MapScreenVC.swift
+++ Restaurants View/MapScreenVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit // to show map
 import CoreLocation // to show user location
 
-class MapScreenVC: UIViewController, MKMapViewDelegate {
+class MapScreenVC: UIViewController {
     var allRestaurants: AllRestaurants?
     
     @IBOutlet weak var mapView: MKMapView!
@@ -19,12 +19,15 @@ class MapScreenVC: UIViewController, MKMapViewDelegate {
     let locationManager = CLLocationManager()  // setup location manager reference and initialize
     let regionInMeters: Double = 10000 // substituting int input since its going to be used a lot
     
+    var previousLocation: CLLocation?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
+        locationManager.delegate = self
         mapView.delegate = self
-    }
+        }
     
     
     func setupLocationManager() {
@@ -44,7 +47,7 @@ class MapScreenVC: UIViewController, MKMapViewDelegate {
         if CLLocationManager.locationServicesEnabled() { // turned on
             setupLocationManager()  //loads data
             checkLocationAuthorization() // runs location authorization
-        } else {  //location servicdes turned off
+        } else {  //location services turned off
             
         }
     }
@@ -52,15 +55,14 @@ class MapScreenVC: UIViewController, MKMapViewDelegate {
     func checkLocationAuthorization() {  // setting permission for when the app can access a user's location
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:  // user authorizes location services when map is in use
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()  //will zoom in on userlocation based on long and lat value
-            break
+            startTrackingUserLocation()
         case .denied: // user denies location services when map is in use
             // if users click deny, you want to show them some notif to show them how to turn it on
             // if user clicks deny, there is no way to prompt them to turn it back on
             break
         case .notDetermined:
             // ask for permission
+            locationManager.requestWhenInUseAuthorization()
             break
         case .restricted: // cannot change status
             // show user alert to show them its restricted
@@ -71,19 +73,66 @@ class MapScreenVC: UIViewController, MKMapViewDelegate {
             break
         }
     }
+    
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()  //will zoom in on userlocation based on long and lat value
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
 }
 
 
 extension MapScreenVC: CLLocationManagerDelegate { // conforming to delegate
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {  // users last known location everytime the user's location updates
-        guard let location = locations.last else {return} //guards against no (nil) location
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)   // how wide the view is
-        mapView.setRegion(region, animated: true)
-    }
     
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization() // if user allows location tracking, the location Manager activates and the map will automatically be zoomed in
+    }
+}
+
+
+extension MapScreenVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) {[weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error {
+                //TODO: Show alert informing the user
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                //TODO: Show alert informing the user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            let postalCode = placemark.postalCode ?? ""
+            let region = placemark.subLocality ?? ""
+            
+            print(streetName)
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = " \(streetNumber) \(streetName), \(region) \(postalCode)"
+            }
+        }
     }
 }
