10ad83fdb9faec2579748502796c3e9700835d6b
David <lin.david985@gmail.com>
2019-12-25 23:15:50 -0500

    updated claim offers button

---
+++ Restaurants View/ClaimCell.swift
@@ -0,0 +1,16 @@
+//
+//  ClaimCell.swift
+//  SNACC
+//
+//  Created by David Lin on 12/25/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class ClaimCell: UITableViewCell {
+    @IBOutlet weak var claimCellLabel: UILabel!
+    @IBOutlet weak var claimButton: UIButton!
+    @IBOutlet weak var requiredStampsLabel: UILabel!
+    
+}
