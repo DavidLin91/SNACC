a8c2a17bb1ca2cacd89fdc79531f7772ba61b3ca
David <lin.david985@gmail.com>
2019-12-28 23:47:22 -0500

    creating login screens

---
+++ Login/RecoverPasswordVC.swift
@@ -0,0 +1,19 @@
+//
+//  RecoverPasswordVC.swift
+//  SNACC
+//
+//  Created by David Lin on 12/28/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class RecoverPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Login/SplashVC.swift
@@ -0,0 +1,18 @@
+//
+//  SplashVC.swift
+//  SNACC
+//
+//  Created by David Lin on 12/28/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
