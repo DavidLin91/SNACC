beb1c691ea7c0a6d15e861744751f7e775f09bd3
David <lin.david985@gmail.com>
2019-12-31 16:25:36 -0500

    created restaurant struct for firebase model

--- SNACC/Models/Restaurants.swift
+++ SNACC/Models/Restaurants.swift
@@ -9,5 +9,26 @@
 import Foundation
 
 struct Restaurants: Codable {
-    
+    let name: String
+    let location: Location
+    let phone: String
+    let hours: Hours
+}
+
+struct Location: Codable {
+    let address: String
+    let borough: String
+    let neighborhood: String
+    let long: Double
+    let lat: Double
+}
+
+struct Hours: Codable {
+    let monday: String
+    let tuesday: String
+    let wednesday: String
+    let thursday: String
+    let friday: String
+    let saturday: String
+    let sunday: String
 }
